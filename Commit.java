574c5009cc4820fc2adff4b5645000af2d76d1d4
hopeloop <lizhaoxic@outlook.com>
2020-12-16 23:16:45 +0800

    生成新的Commit

---
+++ Commit.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class Commit extends ObjectStore{
+    String msg;
+    String timeStamp;
+    String lastTreeKey;
+    String latestTreeKey;
+    String lastCommitKey;
+    String latestCommitKey;
+
+    Commit() throws Exception {
+        setType("Commit");
+        doTimeStamp();
+        Scanner input = new Scanner(System.in);
+        System.out.println("输入备注");
+        this.msg = input.nextLine();
+        if(isCommitable())
+            createCommit();
+    }
+
+    //生成TimeStamp
+    private void doTimeStamp(){
+        Timestamp time =new Timestamp(System.currentTimeMillis());
+        this.timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(time);
+    }
+
+    private void updateHead() throws Exception {
+        File Head = new File(path+File.pathSeparator+"Head");
+        //如果还没有Head文件，则新生成一个
+        if(!Head.exists()){
+            Head.createNewFile();
+        }
+        //以覆盖的方式向文件中写入最新Commit的key
+        FileWriter fw = new FileWriter(Head,false);
+        fw.write(latestCommitKey);
+        fw.flush();
+        fw.close();
+    }
+
+    protected boolean isCommitable() throws Exception {
+        File Head = new File(path+File.pathSeparator+"Head");
+        //如果Head存在，则取出Commit key，进而取出上一次的tree key
+        if(Head.exists()){
+            FileReader fr = new FileReader(Head);
+            char[] head = new char[20];
+            fr.read(head);
+            lastCommitKey = String.valueOf(head);
+            //读取treekey
+            FileReader fr2 = new FileReader(getValue(lastCommitKey));
+            char[] treekey = new char[45];
+            fr2.read(treekey,4,40);
+            lastTreeKey = String.valueOf(treekey);
+        }
+
+
+        latestTreeKey = new Tree(resourcePath).getKey();
+        if(latestTreeKey.equals(lastCommitKey)){
+            System.out.println("仓库无变化！");
+            return false;
+        }
+        else
+            return true;
+    }
+
+    protected void createCommit() throws Exception {
+        File commit = new File(path);
+        FileWriter fr1 = new FileWriter(commit);
+        fr1.write("tree "+latestTreeKey+"\n");
+        //如果有parent commit 则写入，没有则跳过这行
+        if(!lastCommitKey.isEmpty())
+            fr1.append("parent "+lastCommitKey+"\n");
+        fr1.append("Time: "+timeStamp);
+        fr1.append(msg);
+
+        //计算新commit的key,并命其为文件名
+        latestCommitKey = new Hash(commit).getHashcode();
+        commit.renameTo(new File(latestCommitKey));
+        //更新Head
+        updateHead();
+    }
+}
--- ObjectStore.java
+++ ObjectStore.java
@@ -1,44 +1,63 @@
-
 import java.io.*;
 
-public class Blob {
-    private String path;
+public class KeyValueStore {
+    protected String path;
+    private String key;  //object的key值
+    private String type; //object的类型
+    private String name;
 
-    //如果用户没有指定存储位置，则在根目录下新建两层文件夹jGit/object，用于存储生成的blob文件
-    Blob() {
+    //如果用户没有指定存储位置，则在根目录下新建两层文件夹jGit/object，用于存储object
+    KeyValueStore() {
         File file = new File("jGit/object");
-        System.out.println("创建object文件夹：" + file.mkdirs());
+        file.mkdirs();
         path = "./jGit/object";
     }
 
     //也可使用用户指定的存储地点
-    Blob(String user_assigned_path) {
+    KeyValueStore(String user_assigned_path) {
         path = user_assigned_path;
     }
 
+    public void setType(String t){
+            this.type = t;
+    }
+
+    public String getType(){
+            return this.type;
+    }
+
+    public String createKey(File file) throws Exception{
+        Hash hash = new Hash(file);
+        this.key=hash.getHashcode();
+        return this.key;
+    }
+
+    public String getKey(){
+            return this.key;
+    }
 
     /**
-     * 在path目录下创建blob文件（filename:源文件hash值 content:源文件内容）
+     * 在path目录下创建object文件（filename:源文件hash值 content:源文件内容）
      * @param resourceFilePath
      * @throws Exception
      */
-    public void createBlobFile(String resourceFilePath) throws Exception {
+    public void copyFile(String resourceFilePath) throws Exception {
         //创建源文件的File对象
         File file = new File(resourceFilePath);
-        Hash hash = new Hash(file);
+        name = file.getName();
 
-        //如果已经有对应的blob，那就不需要创建新文件，直接返回
-        if (existOrNot(hash.getHashcode()))
+        //如果源文件已创建过object，那就不需要创建新object，直接返回
+        if (existOrNot(createKey(file)))
             return;
         else {
             //在path地址处创建名字为hash值的文件
-            File blobFile = new File(path, hash.getHashcode());
+            File KVFile = new File(path, key);
             //建立文件缓冲区
             byte[] buffer = new byte[1024];
             //从源文件中读取字节，创建FileInputStream对象
             FileInputStream is = new FileInputStream(file);
             //从文件名建立一个FileOutputStream
-            FileOutputStream out = new FileOutputStream(blobFile);
+            FileOutputStream out = new FileOutputStream(KVFile);
             int numRead = 0;
             do {
                 numRead = is.read(buffer);
@@ -51,14 +70,13 @@ public class Blob {
         }
     }
 
-    //(内部方法)传入blob文件名（哈希值），判断待创建的blob文件是否已存在
-    private boolean existOrNot(String blobName) {
+    //传入文件名key（哈希值），判断待创建的blob文件是否已存在
+    private boolean existOrNot(String key) {
             File f = new File(path);
             //列出存储位置的所有文件，遍历一遍，如果有文件名与待创建的文件名相同则返回true
             File[] files = f.listFiles();
             for (File file : files) {
-                if (file.getName().equals(blobName)) {
-                    System.out.println("重复文件");
+                if (file.getName().equals(this.key)) {
                     return true;
                 }
             }
@@ -66,7 +84,7 @@ public class Blob {
         }
 
     /**
-     * （可外部调用）给定key也就是hash值，返回文件的绝对地址
+     * （可外部调用）给定key也就是hash值，返回文件
      * @param key
      */
     public File getValue(String key) {
@@ -74,10 +92,12 @@ public class Blob {
         File[] files = file.listFiles();
         for (File KVFile : files)
             if (KVFile.getName().equals(key)) {
-                System.out.println("文件地址如下：");
-                //返回该文件的绝对地址
                 return KVFile;
             }
         return (new File(path,"null"));
+    }
+
+    public String getName() {
+        return name;
     }
 }
